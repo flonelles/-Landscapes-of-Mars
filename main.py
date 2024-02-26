faaada39af75ff6606135c682bff644d8aae514b
Warlock2006 <tron890.varlakov@yande>
2024-02-26 18:12:42 +0500

    задача Добавляем капитана

--- main.py
+++ main.py
@@ -1,66 +1,43 @@
-from flask import Flask, url_for
+from data.users import *
+from flask import Flask
+from data.db_session import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-def mission():
-    return 'Миссия Колонизация Марса'
-
-
-@app.route('/index')
-def index():
-    return "И на Марсе будут яблони цвести!"
-
-
-@app.route('/promotion')
-def promotion():
-    return '</br>'.join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                         'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!'])
-
-
-@app.route('/image_mars')
-def image_mars():
-    return f"""<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <title>Привет, Марс!</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <img src="{url_for('static', filename='img/mars.jpg')}" width="100" height="111"
-                         alt="здесь должна была быть картинка, но не нашлась">
-                        <p>Вот она какая, красная планета</p>
-                      </body>
-                    </html>"""
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    return f"""<!doctype html>
-                    <html lang="en">
-                      <head>
-                        <meta charset="utf-8">
-                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-                        rel="stylesheet" 
-                        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-                        crossorigin="anonymous">
-                        <title>Колонизация</title>
-                      </head>
-                      <body>
-                        <h1>Жди нас, Марс!</h1>
-                        <img src="{url_for('static', filename='img/mars.jpg')}" width="100" height="111"
-                         alt="здесь должна была быть картинка, но не нашлась">
-                        <div class="alert alert-dark" role="alert">Человечество вырастает из детства.</div>
-                        <div class="alert alert-success" role="alert">Человечеству мала одна планета.</div>
-                        <div class="alert alert-secondary" role="alert">
-                        Мы сделаем обитаемыми безжизненные пока планеты</div>
-                        <div class="alert alert-warning" role="alert">И начнём с Марса!</div>
-                        <div class="alert alert-danger" role="alert">Присоединяйся!</div>
-                      </body>
-                    </html>"""
+def main():
+    global_init('db/mars_explorer.db')
+    session = create_session()
+    user2 = User()
+    user2.surname = "Scott"
+    user2.name = "Ridley"
+    user2.age = 21
+    user2.position = "captain"
+    user2.speciality = "research engineer"
+    user2.address = "module_1"
+    user2.email = "scott_chief@mars.org"
+    session.add(user2)
+    user3 = User()
+    user3.surname = "Jim"
+    user3.name = "Carry"
+    user3.age = 20
+    user3.position = "boatswain"
+    user3.speciality = "botanist"
+    user3.address = "module_2"
+    user3.email = "jim_carry@mars.org"
+    session.add(user3)
+    user4 = User()
+    user4.surname = "Jonn"
+    user4.name = "leon"
+    user4.age = 25
+    user4.position = "cook"
+    user4.speciality = "builder"
+    user4.address = "module_3"
+    user4.email = "jonn_leon@mars.org"
+    session.add(user4)
+    session.commit()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
